527f190cb69fc72d85d1660324df3b1493648a4a
Danilo Sanchez <danilo_das16@hotmamil.com>
2015-06-30 14:07:34 -0500

    Estado inicial del proyecto

---
+++ Morpheus/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from Cultivo import *
+from ProyectoIOT import *
+from Riego import *
+from Seguridad import *
+from Sensores import *
+
+# Create your models here.
---
+++ Morpheus/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from Cultivo import *
+from ProyectoIOT import *
+from Riego import *
+from Seguridad import *
+from Sensores import *
+
+# Create your views here.
